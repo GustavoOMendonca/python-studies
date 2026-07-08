2dc4b02a90eb347bba76f9bdbfee188c0e9cf0f2
GustavoOMendonca <gustavodeoliveira3443@gmail.com>
2026-07-08 09:37:50 -0300

    cria resolução para a ATV_import

---
+++ ATV_import/ferramentas/cripto.py
@@ -0,0 +1,4 @@
+# Criar função que escreva "Chave criada com sucesso!" ao ser chamado
+
+def gerador_de_chaves():
+    print("Chave criada com sucesso!")
---
+++ ATV_import/main.py
@@ -0,0 +1,2 @@
+from ferramentas.cripto import gerador_de_chaves as gerador
+gerador()
